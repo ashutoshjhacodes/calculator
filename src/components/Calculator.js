27061ec07dd97c6c2d1aa660caf40ed89e8ca30a
ashutoshjha23 <jashutosh498@gmail.com>
2023-09-06 12:12:55 +0530

    first commit

---
+++ src/components/Calculator.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Calculator = () => {
+  return (
+    <div className='calculator-grid'>
+      <div className='result'>
+        <div className='prev'></div>
+        <div className='current'></div>
+      </div>
+<button className='span'
+    </div>
+  )
+}
+
+export default Calculator;
